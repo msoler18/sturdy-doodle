fd5e91d2409eee91fb7795b51e945d3eb0487f8c
msoler18 <msoler735@gmail.com>
2025-11-24 13:46:17 -0500

    chore(git): configure commitlint for conventional commits

---
+++ .commitlintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'ci', 'revert'],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'never', ['upper-case', 'pascal-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
+  },
+};
